2d81fe610c78e7e9c3fe5ea9357ad9f4ef7086b2
Cedrik Hoffmann <dev@choffmann.io>
2025-02-09 00:22:50 +0100

    feat: expand ui

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,74 @@
-// import { useState } from 'react'
+import { QueryClient, QueryClientProvider, useMutation, useSuspenseQuery } from "@tanstack/react-query"
+import { Suspense } from "react"
+
+const queryClient = new QueryClient()
 
 function App() {
-  // const [count, setCount] = useState(0)
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <Suspense fallback={"loading..."}>
+            <ControlPanel />
+          </Suspense>
+        </div>
+      </QueryClientProvider>
+    </>
+  )
+}
+
+interface PluginServerInfo {
+  syncInterval: string
+  lastSync: Date
+  slug: string
+  version: string
+}
+
+const ControlPanel = () => {
+  const { data } = useSuspenseQuery<PluginServerInfo>({
+    queryKey: ['info'],
+    queryFn: async () => {
+      return fetch("/api-local/v1/plugin/tbz-baumkataster/info")
+        .then(res => {
+          if (res.status >= 400) {
+            throw res.json()
+          }
+          return res.json()
+        })
+        .then(data => ({
+          syncInterval: data.sync_interval,
+          lastSync: new Date(data.last_sync),
+          slug: data.slug,
+          version: data.version
+        }))
+    }
+  })
+
+  const mutation = useMutation({
+    mutationFn: async () => {
+      return fetch("/api-local/v1/plugin/tbz-baumkataster/sync", {
+        method: "POST"
+      }).then(res => {
+        if (res.status >= 400) {
+          throw res.json()
+        }
+        return
+      })
+    },
+    onSuccess: () => console.log("done!"),
+    onError: (error, variables, context) => console.error("error", error, variables, context),
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['info'] })
+    },
+  })
 
   return (
     <>
-      <div>
-        Huhu
-      </div>
+      <button onClick={() => mutation.mutate()}>
+        Sync manuell
+      </button>
+
+      <p>Last Sync: {data.lastSync.toLocaleString()}</p>
     </>
   )
 }
